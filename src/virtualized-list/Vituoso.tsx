f23e6f70bffe90d68f68e1162cef1325753574bf
Tsuyoshi HARA <chloe463@users.noreply.github.com>
2024-08-13 15:36:11 +0900

    refactor: Make common styles (#3)

--- src/virtualized-list/Vituoso.tsx
+++ src/virtualized-list/Vituoso.tsx
@@ -1,8 +1,8 @@
-import styled from "styled-components";
 import { Virtuoso as VirtuosoList, type ListRange } from "react-virtuoso";
 
 import { Layout } from "../Layout";
 import { useDataSet } from "./useDataSet";
+import { ListWrapper, Card } from "./styles";
 
 export const Virtuoso = () => {
   const { loading, dataSet, appendData } = useDataSet();
@@ -19,7 +19,7 @@ export const Virtuoso = () => {
     <Layout>
       {/* cf. https://virtuoso.dev/window-scrolling/ */}
       {/* cf. https://virtuoso.dev/endless-scrolling/ */}
-      <Wrapper>
+      <ListWrapper>
         <VirtuosoList
           useWindowScroll
           data={dataSet}
@@ -28,34 +28,7 @@ export const Virtuoso = () => {
           itemContent={(index) => <Card key={index}>{dataSet[index]}</Card>}
         />
         {loading && <div>Loading...</div>}
-      </Wrapper>
+      </ListWrapper>
     </Layout>
   );
 };
-
-const Wrapper = styled.div`
-  display: block;
-  box-sizing: content-box;
-  padding: 200px;
-  & ul {
-    position: relative;
-    display: flex;
-    flex-direction: column;
-    gap: 16px;
-    margin: 0;
-    padding: 0;
-    list-style: none;
-  }
-
-  & ul li {
-    margin-top: 16px;
-  }
-`;
-
-const Card = styled.div`
-  display: block;
-  margin-top: 16px;
-  padding: 16px;
-  box-shadow: 0px 0px 0px 1px rgba(0, 0, 0, 0.02),
-    0px 2px 6px 0px rgba(0, 0, 0, 0.1); /* Elevation 4 */
-`;
