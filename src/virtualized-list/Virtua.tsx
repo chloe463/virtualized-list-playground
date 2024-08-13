7c94bd712933c02822111787f9acd3a247ffdeb7
Tsuyoshi HARA <chloe463+git@gmail.com>
2024-08-13 15:35:47 +0900

    refactor: Make common styles

--- src/virtualized-list/Virtua.tsx
+++ src/virtualized-list/Virtua.tsx
@@ -1,8 +1,8 @@
-import styled from "styled-components";
 import { WindowVirtualizer } from "virtua";
 
 import { Layout } from "../Layout";
 import { useDataSet } from "./useDataSet";
+import { ListWrapper, Card } from "./styles";
 
 export const Virtua = () => {
   const { loading, dataSet, appendData } = useDataSet();
@@ -18,7 +18,7 @@ export const Virtua = () => {
   return (
     <Layout>
       {/* cf. https://github.com/inokawa/virtua#window-scroll */}
-      <Wrapper>
+      <ListWrapper>
         <WindowVirtualizer
           onRangeChange={handleRangeChange}
           as={"ul"}
@@ -30,34 +30,7 @@ export const Virtua = () => {
           ))}
         </WindowVirtualizer>
         {loading && <div>Loading...</div>}
-      </Wrapper>
+      </ListWrapper>
     </Layout>
   );
 };
-
-const Wrapper = styled.div`
-  display: block;
-  box-sizing: content-box;
-  padding: 200px;
-  & ul {
-    position: relative;
-    display: flex;
-    flex-direction: column;
-    gap: 16px;
-    margin: 0;
-    padding: 0;
-    list-style: none;
-  }
-
-  & ul li {
-    margin-top: 16px;
-  }
-`;
-
-const Card = styled.div`
-  display: block;
-  margin-top: 16px;
-  padding: 16px;
-  box-shadow: 0px 0px 0px 1px rgba(0, 0, 0, 0.02),
-    0px 2px 6px 0px rgba(0, 0, 0, 0.1); /* Elevation 4 */
-`;
