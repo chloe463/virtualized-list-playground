245158f0122b413e80d7dcbbe5a1c4bc1e17f4e4
Tsuyoshi HARA <chloe463+git@gmail.com>
2024-08-13 13:46:00 +0900

    fix(Virtua): Apply some style

--- src/virtualized-list/Virtua.tsx
+++ src/virtualized-list/Virtua.tsx
@@ -1,3 +1,4 @@
+import styled from "styled-components";
 import { WindowVirtualizer } from "virtua";
 
 import { Layout } from "../Layout";
@@ -17,23 +18,46 @@ export const Virtua = () => {
   return (
     <Layout>
       {/* cf. https://github.com/inokawa/virtua#window-scroll */}
-      <div style={{ padding: 200 }}>
-        <WindowVirtualizer onRangeChange={handleRangeChange}>
+      <Wrapper>
+        <WindowVirtualizer
+          onRangeChange={handleRangeChange}
+          as={"ul"}
+          item={"li"}
+          itemSize={dataSet.length}
+        >
           {Array.from({ length: dataSet.length }).map((_, i) => (
-            <div
-              key={i}
-              style={{
-                // height: Math.floor(Math.random() * 10) * 10 + 10,
-                borderBottom: "solid 1px gray",
-                background: "white",
-              }}
-            >
-              {dataSet[i]}
-            </div>
+            <Card key={i}>{dataSet[i]}</Card>
           ))}
         </WindowVirtualizer>
         {loading && <div>Loading...</div>}
-      </div>
+      </Wrapper>
     </Layout>
   );
 };
+
+const Wrapper = styled.div`
+  display: block;
+  box-sizing: content-box;
+  padding: 200px;
+  & ul {
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+    margin: 0;
+    padding: 0;
+    list-style: none;
+  }
+
+  & ul li {
+    margin-top: 16px;
+  }
+`;
+
+const Card = styled.div`
+  display: block;
+  margin-top: 16px;
+  padding: 16px;
+  box-shadow: 0px 0px 0px 1px rgba(0, 0, 0, 0.02),
+    0px 2px 6px 0px rgba(0, 0, 0, 0.1); /* Elevation 4 */
+`;
